06294345dff6d9f8bed3aaf95ec9f678673d12cd
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-06-30 15:08:08 +0900

    no message

---
+++ Assets/Packages/SequenceCapture/Examples/SequenceCaptureTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SequenceCaptureSystem {
+
+    public class SequenceCaptureTest : MonoBehaviour {
+        public const float SEED = 1000f;
+        public const float DEG = 360f;
+        public float scale = 1f;
+        public float freq = 0.1f;
+        public float angularFreq = 0.1f;
+
+        Vector3 _seed;
+
+        void Start() {
+            _seed = SEED * new Vector3 (Random.value, Random.value, Random.value);
+        }
+    	void Update () {
+            var tpos = Time.timeSinceLevelLoad * freq;
+            transform.position = scale * new Vector3 (
+                Noise (tpos + _seed.x, _seed.y), Noise (tpos + _seed.y, _seed.z), Noise (tpos + _seed.z, _seed.x));
+
+            var trot = Time.timeSinceLevelLoad * angularFreq;
+            transform.localRotation = Quaternion.Euler (
+                DEG * Noise (_seed.y, trot + _seed.z), DEG * Noise (_seed.z, trot + _seed.x), DEG * Noise (_seed.x, trot + _seed.y));
+    	}
+
+        float Noise(float x, float y) {
+            return 2f * Mathf.PerlinNoise (x, y) - 1f;
+        }
+    }
+}
---
+++ Assets/Packages/SequenceCapture/SequenceCapture.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+namespace SequenceCaptureSystem {
+
+    [RequireComponent(typeof(Camera))]
+    public class SequenceCapture : MonoBehaviour {
+        public System.Environment.SpecialFolder saveFolder = System.Environment.SpecialFolder.MyPictures;
+        public int fps = 30;
+
+        string _fileformat;
+        Texture2D _tex;
+
+        void OnEnable() {
+            _tex = new Texture2D (0, 0, TextureFormat.ARGB32, false);
+            Time.captureFramerate = fps;
+            _fileformat = string.Format("{0}_{{0:D5}}.jpg", SceneManager.GetActiveScene().name);
+        }
+        void OnDisable() {
+            Time.captureFramerate = 0;
+            Destroy (_tex);
+        }
+    	void OnPostRender() {
+            try {
+                var w = Screen.width;
+                var h = Screen.height;
+                _tex.Resize (w, h);
+                _tex.ReadPixels (new Rect (0f, 0f, w, h), 0, 0);
+                var path = Path.Combine (
+                    System.Environment.GetFolderPath (saveFolder),
+                    string.Format (_fileformat, Time.frameCount));
+                File.WriteAllBytes (path, _tex.EncodeToJPG ());
+            } catch (System.Exception e) {
+                Debug.LogError (e);
+            }
+    	}
+    }
+}
